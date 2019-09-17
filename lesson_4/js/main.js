f90d05d8c58a578f68f2505452529c4ced32d6d1
justinslv <justinslatvia@gmail.com>
2019-09-17 21:27:43 +0300

    lesson4
    iesākām

---
+++ lesson_4/js/main.js
@@ -0,0 +1,15 @@
+// document.getElementById('some-p').style.color = 'red';
+
+document
+    .getElementById('some-button')
+    .addEventListener('click', function() {
+        document.getElementById('some-p').style.color = 'red';
+    });
+
+
+
+
+
+
+
+
