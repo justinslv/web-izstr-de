1250a7f3e010d309eeecbbf8c9e44126bbb403cc
justinslv <justinslatvia@gmail.com>
2019-09-27 18:39:01 +0300

    last one
    rip page

--- gala darbs/main.js
+++ gala darbs/main.js
@@ -1,3 +1,9 @@
 $('.carousel').carousel({
-    interval: 4000
+    interval: 7000
+  })
+
+  $('body').scrollspy({ target: '#navbar-example' })
+
+  $('[data-spy="scroll"]').each(function () {
+    var $spy = $(this).scrollspy('refresh')
   })
